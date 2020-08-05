3fc75655a1d6597feff74df10a632a03ec20e90e
Ian da Conceição da Silva <iantorquato2@gmail.com>
2020-08-05 13:13:48 -0300

    Correção de erro

--- web/src/components/Select/index.tsx
+++ web/src/components/Select/index.tsx
@@ -15,7 +15,7 @@ const Select: React.FC<SelectProps> = ({ label, name, options, ...rest }) => {
   return (
     <div className="select-block">
       <label htmlFor={name}>{label}</label>
-      <select id={name} defaultValue="" {...rest}>
+      <select id={name} value="" {...rest}>
         <option value="" disabled hidden>Selecione uma opção</option>
 
         {options.map(option => <option value={option.value} key={option.value}>{option.label}</option>)}

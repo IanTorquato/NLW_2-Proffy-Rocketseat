5638ca326e43453e90be656fce8b7deaaad500bb
Ian da Conceição da Silva <iantorquato2@gmail.com>
2020-08-05 11:15:59 -0300

    Seleção de data/horário dentro do form

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Header from '../../components/Header'
 import Input from '../../components/Input'
@@ -9,6 +9,12 @@ import iconWarning from '../../assets/images/icons/warning.svg'
 import './styles.css'
 
 const TeacherForm: React.FC = () => {
+  const [scheduleItems, setScheduleItems] = useState([{ week_day: 0, from: '11:00 AM', to: '2:00 PM' }])
+
+  function addNewScheduleItem() {
+    setScheduleItems([...scheduleItems, { week_day: 0, from: '11:00 AM', to: '2:00 PM' }])
+  }
+
   return (
     <div id="page-teacher-form" className="container">
       <Header
@@ -37,6 +43,29 @@ const TeacherForm: React.FC = () => {
           ]} />
 
           <Input label="Custo da sua hora por aula" name="cost" />
+        </fieldset>
+
+        <fieldset>
+          <legend>
+            Horários disponíveis
+            <button type="button" onClick={addNewScheduleItem}>+ Novo horário</button>
+          </legend>
+
+          {scheduleItems.map(scheduleItem => (
+            <div className="schedule-item" key={scheduleItem.week_day}>
+              <Select label="Dia da semana" name="week_day" options={[
+                { value: '0', label: 'Domingo' },
+                { value: '1', label: 'Segunda-feira' },
+                { value: '2', label: 'Terça-feira' },
+                { value: '3', label: 'Quarta-feira' },
+                { value: '4', label: 'Quinta-feira' },
+                { value: '5', label: 'Sexta-feira' },
+                { value: '6', label: 'Sábado' }
+              ]} />
+              <Input label="Das" name="from" type="time" />
+              <Input label="Até" name="to" type="time" />
+            </div>
+          ))}
         </fieldset>
 
         <footer>
